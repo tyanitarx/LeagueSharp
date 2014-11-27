280f2c457d5d93e8f454a7ef12eadc1a264447f2
tyanitarx <demonlit1@hotmail.com>
2014-11-27 16:26:13 -0500

    Create Program.cs

---
+++ Auto-Attack-Reset/Auto-Attack-Reset/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto_Attack_Reset
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            new AttackReset();
+        }
+    }
+}
